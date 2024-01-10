934d7a9f8f1518442b673ac2ef2555af94deba6f
jackie <jedoro@uri.edu>
2024-01-09 19:20:39 -0500

    kil

---
+++ Beepa/Beepa Watch App/BeepaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeepaApp.swift
+//  Beepa Watch App
+//
+//  Created by Jacqueline Edoro on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Beepa_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Beepa/Beepa Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Beepa Watch App
+//
+//  Created by Jacqueline Edoro on 1/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
